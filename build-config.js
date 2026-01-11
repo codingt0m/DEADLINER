4209036bdd6076c77b9b999f3957a3c7823e51cc
tom <tom.lequeux@icloud.com>
2026-01-11 18:03:29 +0100

    .

--- build-config.js
+++ build-config.js
@@ -13,12 +13,12 @@ if (!fs.existsSync(outputDir)){
 // 2. Génération du contenu de config.js avec les variables d'environnement
 const configContent = `// Fichier généré automatiquement par Vercel
 const firebaseConfig = {
-  apiKey: "${process.env.FIREBASE_API_KEY}",
-  authDomain: "${process.env.FIREBASE_AUTH_DOMAIN}",
-  projectId: "${process.env.FIREBASE_PROJECT_ID}",
-  storageBucket: "${process.env.FIREBASE_STORAGE_BUCKET}",
-  messagingSenderId: "${process.env.FIREBASE_MESSAGING_SENDER_ID}",
-  appId: "${process.env.FIREBASE_APP_ID}"
+  apiKey: "${process.env.apiKey}",
+  authDomain: "${process.env.authDomain}",
+  projectId: "${process.env.projectId}",
+  storageBucket: "${process.env.storageBucket}",
+  messagingSenderId: "${process.env.messagingSenderId}",
+  appId: "${process.env.appId}"
 };
 
 export { firebaseConfig };`;
